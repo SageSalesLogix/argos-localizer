41e4ef34ce06b9cbe7f3e34ed9fc2b53a7660893
Tony Skidmore <tony.skidmore@sage.com>
2011-07-13 14:44:56 -0700

    removed globals, various cleanup

--- argos-localizer.rb
+++ argos-localizer.rb
@@ -1,6 +1,5 @@
 require_relative 'lib/docjs'
-require 'builder'
-BUILDER = Builder::XmlMarkup.new(:indent => 2)
+require 'nokogiri'
 
 def create_localization_worksheet(source, destination)
   inspector = DocJS::Inspectors::ExtJsInspector.new
@@ -8,71 +7,62 @@ def create_localization_worksheet(source, destination)
   sdkPath = source.split('\\')[0..-4].push("argos-sdk").push("src").join("\\");
   inspector = DocJS::Inspectors::ExtJsInspector.new
   sdk = inspector.inspect_path(sdkPath)
-  generate_csv(destination,project,sdk)
+  generate_xml(destination,project,sdk)
 end
 
+def generate_xml(destination, project, sdk)
 
-# Flow: BUILDER is a global xml doc that gets appended to
-# as it loops over each property of each class within the project
-# currently is whitelisted to string properties and properties that
-# end with 'Text' 'Format' 'String'
-def generate_csv(dest, proj, sdk)
-	# add from user project
-	proj.classes do |cls|
-			processObject(cls)
+	xmlDoc = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
+		xml.localization
 	end
+	
+	rootNode = xmlDoc.doc.xpath('//localization').first
+	
+	# add from user project
+	project.classes do |className|
+			process_object(className, rootNode)
+	end
+	
 	# add from SDK
 	if sdk!=nil then
-		sdk.classes do |cls|
-			processObject(cls)
+		sdk.classes do |className|
+			process_object(className, rootNode)
 		end
+		
 	end
 	
 	# write results to file
-	open(dest, 'wt', encoding: 'UTF-8') { |f|
-		f << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-		f << "<localization>\n"
-		f << BUILDER.target!
-		f << "</localization>\n"
+	open(destination, 'wt', encoding: 'UTF-8') { |f|
+		f << xmlDoc.to_xml
 	}
-	print "Finished generating: " + dest
+	
+	print "Finished generating: " + destination
 end 
 
-def endsWithText(val)
-	return (val =~ /Text|String|Format$/) != nil
-end 
-
-def addNode(clss,prp,val)
-	#<data class=”Mobile.SalesLogix.Account.Detail” property=”acctMgrText”>
-	#   <description>Context for the value string below</description>
-	#   <value>Blah Blah String will be Translated</value> 
-	#</data>
-	BUILDER.data("class"=>clss, "property"=>prp) { |b|
-		# placeholder for internal use by localizers
-		b.description("")
-		b.value(val)
-	}
-end
-
-def processObject(obj)
-	obj.properties.each do |property|
-		next if !endsWithText("#{property.name}")
+def process_object(object, rootNode)
+	object.properties.each do |property|
+		next if !ends_with_text("#{property.name}")
+		
 		if "#{property.type}" == "string" then
 			# values come in as 'val', here we strip the ''
-			val = "#{property.value}".slice!(1..-1).chop!()
+			cleanValue = "#{property.value}".slice!(1..-1).chop!()
+			
 		elsif "#{property.type}" == "object" then
-			processSubObject("#{obj.name}","#{property.name}",property.value)
+			process_subobject("#{object.name}","#{property.name}",property.value, rootNode)
 			next
+			
 		else
-			#TODO: handle this error better
+			#TODO: handle not string nor object
 			next
+			
 		end
-		addNode("#{obj.name}","#{property.name}",val)
+		
+		add_node("#{object.name}","#{property.name}", cleanValue, rootNode)
 	end
 end
 
-def processSubObject(name,sub,obj)
-	obj.each do |property, value|
+def process_subobject(className, subName, object, rootNode)
+	object.each do |property, value|
 		# sometimes the keys were defined with int (valid js, not valid json)
 		if property.is_a?(Integer) then
 			property = property.to_s()
@@ -80,22 +70,45 @@ def processSubObject(name,sub,obj)
 		
 		if value.is_a? String then
 			# values come in as 'val', here we strip the ''
-			val = value.slice!(1..-1).chop!()
+			cleanValue = value.slice!(1..-1).chop!()
 			
 		# test if further delving is needed
 		elsif value.is_a? Hash then
-			processSubObject(name, sub+"["+property+"]", value)
+			process_subobject(className, subName+"["+property+"]", value)
+			
 		else
-			#TODO: handle this error better
+			#TODO: handle neither string or hash
 			next
+			
 		end
-		addNode(name,sub+"["+property+"]",val)
+		
+		add_node(className,subName+"["+property+"]", cleanValue, rootNode)
 	end
 end
 
-def process_localization_worksheet(source)
+def add_node(className,property,value,rootNode)
+	#<data class=”Mobile.SalesLogix.Account.Detail” property=”acctMgrText”>
+	#   <description></description>
+	#   <value>String to be Translated</value> 
+	#</data>
+	dataNode = Nokogiri::XML::Node.new("data", rootNode.document())
+	dataNode["class"] = className
+	dataNode["property"] = property
 
+	descriptionNode = Nokogiri::XML::Node.new("description", rootNode.document())
+
+	valueNode = Nokogiri::XML::Node.new("value", rootNode.document())
+	valueNode.content = value
+	
+	dataNode.add_child(descriptionNode)
+	dataNode.add_child(valueNode)
+	
+	rootNode.add_child(dataNode)
 end
+
+def ends_with_text(string)
+	return (string =~ /Text|String|Format$/) != nil
+end 
 
 def parse_cli_arguments(args)
   # todo: process arguments
